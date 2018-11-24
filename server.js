db298c1b26e1f6117c9f0d95aca422dc15ca797a
MyFaith <faith0725@outlook.com>
2018-11-25 00:27:10 +0800

    feat: 优化弹幕和礼物显示样式

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 'use strict';
 
+const pf = require('util').promisify;
+const request = require('request');
 const io = require('socket.io')(8080);
-const Danmu = require('./Danmu');
+const Danmu = require('./lib/Danmu');
+
+const ROOM_ID = 71415;
+
+const danmu = new Danmu(ROOM_ID);
+danmu.start();
 
 io.on('connection', (socket) => {
-    console.log('connected');
-    const danmu = new Danmu(122402);
-    danmu.start();
 
     danmu.on('chat', (data) => {
         io.emit('chat', data);
@@ -18,5 +22,17 @@ io.on('connection', (socket) => {
 
     danmu.on('enter', (data) => {
         io.emit('enter', data);
+    });
+
+    danmu.on('rankup', (data) => {
+       io.emit('enter', data);
+    });
+
+    danmu.on('ban', (data) => {
+        io.emit('ban', data);
+    });
+
+    danmu.on('badgeup', (data) => {
+       io.emit('badgeup', data);
     });
 });
