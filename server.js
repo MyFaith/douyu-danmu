6e431b275c088d0bf7a283ddf885a5131fcfffc2
MyFaith <faith0725@outlook.com>
2018-11-26 00:02:31 +0800

    feat: 修改弹幕列表样式，增加进入房间和升级消息

--- server.js
+++ server.js
@@ -5,34 +5,32 @@ const request = require('request');
 const io = require('socket.io')(8080);
 const Danmu = require('./lib/Danmu');
 
-const ROOM_ID = 71415;
-
-const danmu = new Danmu(ROOM_ID);
-danmu.start();
+const ROOM_ID = 1975380;
 
 io.on('connection', (socket) => {
-
+    /**
+     * Bugs
+     * 第二次连接的时候会返回两条消息，以此类推。
+     */
+    
+    const danmu = new Danmu(ROOM_ID);
+    danmu.start();
     danmu.on('chat', (data) => {
         io.emit('chat', data);
     });
-
     danmu.on('gift', (data) => {
         io.emit('gift', data);
     });
-
     danmu.on('enter', (data) => {
         io.emit('enter', data);
     });
-
     danmu.on('rankup', (data) => {
-       io.emit('enter', data);
+        io.emit('enter', data);
     });
-
     danmu.on('ban', (data) => {
         io.emit('ban', data);
     });
-
     danmu.on('badgeup', (data) => {
-       io.emit('badgeup', data);
+        io.emit('badgeup', data);
     });
 });
