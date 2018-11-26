009d2c39bf35ff8f36fe485f2b1722a429bc9825
MyFaith <faith0725@outlook.com>
2018-11-26 23:18:02 +0800

    🐛修复多次连接socket弹幕返回重复的问题

--- server.js
+++ server.js
@@ -5,32 +5,33 @@ const request = require('request');
 const io = require('socket.io')(8080);
 const Danmu = require('./lib/Danmu');
 
-const ROOM_ID = 1975380;
+let danmu = null;
 
 io.on('connection', (socket) => {
-    /**
-     * Bugs
-     * 第二次连接的时候会返回两条消息，以此类推。
-     */
-    
-    const danmu = new Danmu(ROOM_ID);
-    danmu.start();
-    danmu.on('chat', (data) => {
-        io.emit('chat', data);
-    });
-    danmu.on('gift', (data) => {
-        io.emit('gift', data);
-    });
-    danmu.on('enter', (data) => {
-        io.emit('enter', data);
-    });
-    danmu.on('rankup', (data) => {
-        io.emit('enter', data);
-    });
-    danmu.on('ban', (data) => {
-        io.emit('ban', data);
-    });
-    danmu.on('badgeup', (data) => {
-        io.emit('badgeup', data);
-    });
+    // 获取房间号
+    let roomId = parseInt(socket.handshake.query.roomId);
+    // 初始化弹幕
+    if(!danmu){
+        danmu = new Danmu(roomId);
+        danmu.start();
+        // 监听
+        danmu.on('chat', (data) => {
+            io.emit('chat', data);
+        });
+        danmu.on('gift', (data) => {
+            io.emit('gift', data);
+        });
+        danmu.on('enter', (data) => {
+            io.emit('enter', data);
+        });
+        danmu.on('rankup', (data) => {
+            io.emit('rankup', data);
+        });
+        danmu.on('ban', (data) => {
+            io.emit('ban', data);
+        });
+        danmu.on('badgeup', (data) => {
+            io.emit('badgeup', data);
+        });
+    }
 });
